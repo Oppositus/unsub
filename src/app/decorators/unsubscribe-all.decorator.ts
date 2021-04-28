66bece4b6dd2cb3275b662a70a100f8bd14b57de
Denis Morosenko <damoroze@mts.ru>
2021-04-28 18:26:25 +0300

    Del comments

--- src/app/decorators/unsubscribe-all.decorator.ts
+++ src/app/decorators/unsubscribe-all.decorator.ts
@@ -1,3 +1,9 @@
+/**
+ * UnsubscribeAll() component decorator to automatically unsubscribe from all subscriptions.
+ *                 unsubscribe will be called when ngOnDestroy() is called
+ *                 if component has ngOnDestroy function it will be called after unsubscribe
+ *                 if component has no ngOnDestroy function it will be automatically created
+ */
 export function UnsubscribeAll(): (constructor: new (...args: any[]) => void) => void {
 
   // tslint:disable-next-line:only-arrow-functions
@@ -7,23 +13,25 @@ export function UnsubscribeAll(): (constructor: new (...args: any[]) => void) =>
 
       // tslint:disable-next-line:only-arrow-functions
       return function(): void {
+
         // @ts-ignore
         Object.values(this)
           .filter((prop: any) => typeof prop.unsubscribe === 'function')
           .forEach((sub: any) => {
             try {
-              console.log('Unsubscribe', sub);
               sub.unsubscribe();
-            } catch (e) { } // nothing;
+            } catch (e) { }
           });
 
         // @ts-ignore
         savedNgOnDestroy.call(this);
       };
+
     }
 
     const ngOnDestroy = constructor.prototype.ngOnDestroy || (() => {});
     constructor.prototype.ngOnDestroy = unsub(ngOnDestroy);
+
   };
 
 }
--- src/app/decorators/unsubscribe.decorator.ts
+++ src/app/decorators/unsubscribe.decorator.ts
@@ -1,58 +1,41 @@
 /**
- * Unsubscribe() decorator to automatically unsubscribe from decorated subscriptions.
+ * Unsubscribe() property decorator to automatically unsubscribe from decorated subscriptions.
  *               unsubscribe will be called when ngOnDestroy() is called
  *               if component has ngOnDestroy function it will be called after unsubscribe
  *               if component has no ngOnDestroy function it will be automatically created
  */
 export function Unsubscribe(): (target: any, key: string) => void {
 
-  // The decorator itself
   // tslint:disable-next-line:only-arrow-functions
   return function(target: any, key: string): void {
 
-    // Function that do unsubscribe and then call ngOnDestroy.
     function unsub(): void {
-      // "this" value is defined by call component.ngOnDestroy()
-
-      // Unsubscribe
-      target.__unsubscribeArray__.forEach((sub: string) => {
+      target.__unsubscribe__.subscriptions.forEach((sub: string) => {
         try {
-          console.log('Unsubscribe from', sub);
           // @ts-ignore
           this[sub].unsubscribe();
-        } catch (e) { } // nothing
+        } catch (e) { }
       });
 
-      // And call ngOnDestroy
       // @ts-ignore
-      target.__savedNgOnDestroy__.call(this);
+      target.__unsubscribe__.ngOnDestroy.call(this);
     }
 
-    // Add __unsubscribeArray__ to target prototype
-    if (target.__unsubscribeArray__ === undefined) {
-      Object.defineProperty(target, '__unsubscribeArray__', {
+    if (target.__unsubscribe__ === undefined) {
+      Object.defineProperty(target, '__unsubscribe__', {
         configurable: false,
         enumerable: false,
         writable: false,
-        value: []
+        value: {
+          subscriptions: [],
+          ngOnDestroy: target.ngOnDestroy || (() => {})
+        }
       });
     }
 
-    // Add __savedNgOnDestroy__ to target prototype with saved ngOnDestroy
-    if (target.__savedNgOnDestroy__ === undefined) {
-      Object.defineProperty(target, '__savedNgOnDestroy__', {
-        configurable: false,
-        enumerable: false,
-        writable: false,
-        value: target.ngOnDestroy || (() => {})
-      });
-    }
-
-    // Push subscription field name to unsubscribe array
-    target.__unsubscribeArray__.push(key);
-
-    // Replace ngOnDestroy. Multiple replace is ok.
+    target.__unsubscribe__.subscriptions.push(key);
     target.ngOnDestroy = unsub;
 
   };
+
 }
