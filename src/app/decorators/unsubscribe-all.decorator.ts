bb6a90de39b23486b033c8c22f81eef9280e0d5a
Denis Morosenko <damoroze@mts.ru>
2021-04-28 16:47:49 +0300

    UnsubscribeAll

--- src/app/decorators/unsubscribe-all.decorator.ts
+++ src/app/decorators/unsubscribe-all.decorator.ts
@@ -4,9 +4,9 @@ export function UnsubscribeAll(): (constructor: new (...args: any[]) => void) =>
   return function(constructor: new (...args: any[]) => void): void {
 
     function unsub(savedNgOnDestroy: () => void): () => void {
+
       // tslint:disable-next-line:only-arrow-functions
       return function(): void {
-
         // @ts-ignore
         Object.values(this)
           .filter((prop: any) => typeof prop.unsubscribe === 'function')
